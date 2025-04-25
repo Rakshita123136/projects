5eaace7a3845611757298e46d804abdcbab3c201
rakshita123136 <jayauppar2@gmail.com>
2025-04-25 21:15:10 +0530

    adding practice file

---
+++ jstutorial/day1.js
@@ -0,0 +1,24 @@
+// Array
+let stu = ["john", "reem", "resh" ,"jane"];
+console.log(stu[0]) //index of the array
+//modify
+console.log("here the modification is done")
+stu[0] = "orton"
+
+//push or adding value
+console.log("here is example of push")
+stu.push("cena")
+
+//pop
+console.log("here is example of pop")
+stu.pop()
+
+//shift
+console.log("here is example of shift")
+stu.shift("jane")
+
+//unshift
+console.log("Here is example of unshift")
+stu.unshift("cena")
+//index of method
+console.log(stu.indexOf("jane"))
