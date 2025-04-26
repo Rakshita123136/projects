ac309d78866f6bfec729bcf0ba9b776483d96475
rakshita123136 <jayauppar2@gmail.com>
2025-04-26 23:01:20 +0530

    adding updated jstutorial

--- jstutorial/day2.js
+++ jstutorial/day2.js
@@ -1,14 +1,28 @@
-//objects
+// Objects and methods
 let person = {
-     name: "john",
-     age : 20,
-     city:"Delhi"
-}
-console.log(person)
-console.log(person.name)
-console.log(person["age"])
+    name: "john",
+    age: 20,
+    city: "Delhi", 
+    greet: function() {
+       console.log("This is function");
+    }
+};
 
+console.log(person.greet()); // calls the greet method
+console.log(person); // prints full object
+console.log(person.name); // dot notation
+console.log(person["age"]); // bracket notation
+
+// Add a new property
 person.gender = "Male";
-console.log(person)
-delete person.city
-console.log("city" in person)
+console.log(person);
+
+// Delete a property
+delete person.city;
+
+// Check if a property exists
+console.log("city" in person);
+ let keys = Object.keys(person)
+ console.log(keys)
+ let values = Object.values(person)
+ console.log(values)
