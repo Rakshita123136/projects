b940ac4d7d7524cee7e39f8608693c94b48d2139
rakshita123136 <jayauppar2@gmail.com>
2025-06-01 22:37:06 +0530

    Updateed Dom

--- jstutorial/Dom/dom1.js
+++ jstutorial/Dom/dom1.js
@@ -13,3 +13,30 @@ console.log(d)
 //queryselectorall()
 var e = document.querySelectorAll('li')
 console.log(e)
+
+//parent elements
+var parent = document.querySelector('ul')
+var p = parent.parentElement;
+console.log(p)
+
+//childern elements
+var child = document.querySelector('ul')
+var p =child.children;
+console.log(p)
+
+//childernnode
+var child = document.querySelector('ul')
+console.log(child.children[0])
+console.log(child.childNodes)
+
+//siblings
+//method-1
+console.log(document.querySelector('.hero').previousElementSibling)
+//method-2
+var a = document.querySelector('.hero')
+if(previousElementSibling == null){
+    console.log("Siblings not present")
+}else{
+console.log(a.previousElementSibling)
+}
+//append
