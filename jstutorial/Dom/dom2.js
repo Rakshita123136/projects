dba76941440b78480cf29d8142fa2c8e3fdc54b4
rakshita123136 <jayauppar2@gmail.com>
2025-06-05 23:47:12 +0530

    mmodified Dom Folder

--- jstutorial/Dom/dom2.js
+++ jstutorial/Dom/dom2.js
@@ -4,3 +4,7 @@ console.log(a)
 var b = document.querySelector('h1').textContent; //jo hidden hota h wo display hota h
 console.log(b)
 
+var target = document.querySelector('.hero')
+var newE = '<b>Drop X out </b>';
+target.insertAdjacentHTML("afterbegin", newE)
+target.insertAdjacentHTML("beforeend",newE)
