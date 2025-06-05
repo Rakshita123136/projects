32043bdf161e7724e650559da76946e7d4beffd7
rakshita123136 <jayauppar2@gmail.com>
2025-06-05 23:10:32 +0530

    updated Dom folder

--- jstutorial/Dom/dom1.js
+++ jstutorial/Dom/dom1.js
@@ -40,3 +40,55 @@ if(previousElementSibling == null){
 console.log(a.previousElementSibling)
 }
 //append
+const ul = document.querySelector('ul');
+const newE = document.createElement('li');
+newE.textContent = 'gigi hadid';
+ul.appendChild(newE)
+//remove
+const ul2 = document.querySelector('li')
+ul.remove();
+//replace child
+const list = document.querySelector('ul');
+const childToChange = list.childern[1]
+const newli =document.createElement('li')
+newli.textContent = 'giga hadid'
+list.replaceChild(newli,childToChange)
+
+//parent node
+var parent2 = document.querySelector('html')
+var p = parent2.parentElement;
+console.log(p)
+console.log("-------")
+var parent2 = document.querySelector('html')
+var q = parent2.parentNode;
+console.log(q)
+
+//previous sibling
+var presib = document.querySelector('.hero');
+console.log(presib.previousSibling);
+
+//next sibling
+var nextsib = document.querySelector('.hero')
+console.log(nextsib.nextSibling);
+console.log(nextsib.nextElementSibling);
+
+//first child
+var fchild = document.querySelector('ul')
+console.log(fchild.firstChild)
+console.log(fchild.firstElementChild)
+
+//last child
+var lchild = document.querySelector('ul')
+console.log(lchild.lastChild)
+console.log(lchild.lastElementChild)
+
+//inner html
+let a = document.querySelector('.hero')
+a.innerHTML = <b>Giga chad</b>
+a.innerHTML += <b>Giga chad</b>
+console.log(a.innerHTML)
+
+//outer html
+let out = document.querySelector('.hero')
+out.outerHTML = '<li><b>giga chad </b> </li>'
+console.log(out.outerHTML)
