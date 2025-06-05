801b7fee3aef36c9e65e1b9ebf1ac347953b2f71
rakshita123136 <jayauppar2@gmail.com>
2025-06-05 23:39:35 +0530

    Initial commit

---
+++ jstutorial/Dom/dom2.js
@@ -0,0 +1,6 @@
+var a = document.querySelector('h1').innerText; //jo hidden hota h ya display none hota wo nhi dekhta
+console.log(a)
+
+var b = document.querySelector('h1').textContent; //jo hidden hota h wo display hota h
+console.log(b)
+
