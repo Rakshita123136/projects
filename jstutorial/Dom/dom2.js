13334764c661142548ffcf6ee95577b115db2687
rakshita123136 <jayauppar2@gmail.com>
2025-06-10 16:42:59 +0530

    adding new html

--- jstutorial/Dom/dom2.js
+++ jstutorial/Dom/dom2.js
@@ -7,4 +7,20 @@ console.log(b)
 var target = document.querySelector('.hero')
 var newE = '<b>Drop X out </b>';
 target.insertAdjacentHTML("afterbegin", newE)
-target.insertAdjacentHTML("beforeend",newE)
+target.insertAdjacentHTML("beforeend",newE);
+//styling
+document.body.style.backgroundColor = "red";
+let a = document.querySelector("h1")
+a.style.color = "red";
+
+//get and set
+let hero = document.querySelector('#hero')
+//console.log(hero.getAttribute('about'))
+hero.setAttribute("example" ,123);
+//console.log(hero.outerHTML);
+
+for(let att of hero.attributes){
+    console.log(`${att.name} = ${att.value}`)
+}
+
+
